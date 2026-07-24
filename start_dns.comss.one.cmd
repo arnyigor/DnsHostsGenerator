e349691687dfbd8c5fdc45bfe710bc244a42750a
Arnyigor <arnyigor@gmail.com>
2026-07-24 14:54:53 +0500

    Clean up legacy project files

--- start_dns.comss.one.cmd
+++
@@ -1,2 +0,0 @@
-call dns-sni-proxy-hosts-generator.cmd dns.comss.one 8.8.8.8 domainlist.txt host_out_dns.comss.one.txt dedup
-rem call dns-sni-proxy-hosts-generator.cmd router.comss.one 8.8.8.8 domainlist.txt host_out_router.comss.one.txt dedup
