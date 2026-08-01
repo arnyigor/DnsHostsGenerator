0a50d46f87bade2547528a94ac80a367482834c3
Arnyigor <arnyigor@gmail.com>
2026-08-01 07:47:02 +0500

    Bump desktop package version to 1.3.0

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -22,7 +22,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Exe, TargetFormat.Msi)
             packageName = "DnsHostsGenerator"
-            packageVersion = "1.2.0"
+            packageVersion = "1.3.0"
             includeAllModules = true
         }
     }
